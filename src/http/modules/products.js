ec5e0ca200e111eebfd52feaba86121df98c8e7d
unknown <Alex>
2023-05-21 01:16:43 +0300

    added test data

--- src/http/modules/products.js
+++ src/http/modules/products.js
@@ -2,49 +2,49 @@ export function loadProducts () {
   return [{
     productName: 'IPhone XS Max',
     productDescription: 'Wonderful solution for wonderful people',
-    productCost: 299,
+    productCost: 499,
     id: 1
   },
   {
-    productName: 'IPhone XS Max',
+    productName: 'IPhone XS ',
     productDescription: 'Wonderful solution for wonderful people',
-    productCost: 299,
+    productCost: 399,
     id: 2
   },
   {
-    productName: 'IPhone XS Max',
+    productName: 'IPhone 13',
     productDescription: 'Wonderful solution for wonderful people',
-    productCost: 299,
+    productCost: 699,
     id: 3
   },
   {
-    productName: 'IPhone XS Max',
+    productName: 'IPhone 13 Pro ',
     productDescription: 'Wonderful solution for wonderful people',
-    productCost: 299,
+    productCost: 899,
     id: 4
   },
   {
-    productName: 'IPhone XS Max',
+    productName: 'IPhone 13 Pro Max',
     productDescription: 'Wonderful solution for wonderful people',
-    productCost: 299,
+    productCost: 999,
     id: 5
   },
   {
-    productName: 'IPhone XS Max',
+    productName: 'IPhone 14',
     productDescription: 'Wonderful solution for wonderful people',
-    productCost: 299,
+    productCost: 1099,
     id: 6
   },
   {
-    productName: 'IPhone XS Max',
+    productName: 'IPhone 14 Pro Max',
     productDescription: 'Wonderful solution for wonderful people',
-    productCost: 299,
+    productCost: 1299,
     id: 7
   },
   {
-    productName: 'IPhone XS Max',
+    productName: 'IPhone 15 XS Max Pro 4S',
     productDescription: 'Wonderful solution for wonderful people',
-    productCost: 299,
+    productCost: 199,
     id: 8
   }]
 }
